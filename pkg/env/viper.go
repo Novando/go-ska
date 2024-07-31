29246c06ede3c6541d6fd608d418c2a360849bbf
rizkynovando <rizkynovando@gmail.com>
2024-07-31 11:03:08 +0000

    feaure: restrucutre redis and pgx

--- pkg/env/viper.go
+++ pkg/env/viper.go
@@ -6,33 +6,30 @@ import (
 	"strings"
 )
 
-const ConfigPath = "./configs"
-
-const (
-	ErrFailedToParseConfig = "Failed to parse config name"
-	ErrFailedToReadConfig  = "Fatal error configs file: %s"
-)
-
-// Init initializes the configuration with the given name.
-//
-// It takes basic name string as basic parameter and does not return anything.
-func Init(name string, logger *logger.Logger) {
-	logger.Infof("Initializing configuration with name: %s", name)
-
-	splitName := strings.Split(name, ".")
-	if len(splitName) != 2 {
-		logger.Fatal(ErrFailedToParseConfig)
+/**
+ * InitViper
+ *
+ * Initialize Viper to use the config file as env variable
+ */ 
+func InitViper(path string, logger logger.Logger) error {
+	var configName string
+	splitPaths := strings.Split(path, "/")
+	if len(splitPaths) > 0 {
+		for i := 0; i < len(splitPaths); i++ {
+			configName = splitPaths[i]
+		}
 	}
-
-	configName := splitName[0]
-	configType := splitName[1]
-
-	viper.SetConfigName(configName)
-	viper.SetConfigType(configType) // REQUIRED if the configs file does not have the extension in the name
-	viper.AddConfigPath(ConfigPath) // optionally look for configs in the working directory
-
+	splitNames := strings.Split(configName, ".")
+	if len(splitNames) < 2 {
+		logger.Fatalf("Failed to parse config name")
+	}
+	formatName := splitNames[len(splitNames)-1]
+	viper.SetConfigName(strings.TrimRight(configName, "."+formatName))
+	viper.SetConfigType(formatName)
+	viper.AddConfigPath(strings.TrimRight(path, configName))
 	err := viper.ReadInConfig()
 	if err != nil {
-		logger.Fatalf(ErrFailedToReadConfig, err)
+		logger.Infof("Configs file: %v", err)
 	}
+	return err
 }
